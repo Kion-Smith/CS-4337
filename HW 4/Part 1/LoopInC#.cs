a7e88420aa77cdc188689768531c61c6fdb88811
Kion-Smith <kion_smith@hotmail.com>
2018-11-01 23:54:35 -0500

    loops in other programs

---
+++ HW 4/Part 1/LoopInC#.cs
@@ -0,0 +1,19 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int j = 0;
+		int i = 0;
+		int k = (j+13)/27;
+		while (k>10)
+		{
+			k=k+1;
+			i=3*k-1;
+			
+		}
+		
+		
+	}
+}
---
+++ HW 4/Part 1/LoopInJava.java
@@ -0,0 +1,27 @@
+/**
+ * @(#)LoopInJava.java
+ *
+ *
+ * @author 
+ * @version 1.00 2018/11/1
+ */
+
+
+public class LoopInJava {
+
+    public static void main(String [] args) 
+    {
+    	int j = 0;
+    	int i = 0;
+    	
+    	int k =(j+13)/27;
+    	while(k>10)
+    	{
+    		k+=1;
+    		i = 3*k-1;
+    	}
+    	
+    }
+    
+    
+}
